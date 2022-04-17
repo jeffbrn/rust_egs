a18064e0dd7496750732d7acaf2104745b4b996f
jeffbrn <jeff.brn@gmail.com>
2022-04-16 20:29:57 -0600

    initial lexer

---
+++ yap/src/tokenizer/state/mod.rs
@@ -0,0 +1,39 @@
+pub struct State {
+  pub emit_token : Option<i32>,
+}
+
+pub struct Transition {
+  min: char,
+  max: char,
+  exception: Option<char>,
+  next_state: i32,
+}
+
+pub struct LexerStates {
+  pub states : Vec<State>,
+  pub transitions : HashMap<i32, Vec<Transition>>,
+}
+
+impl LexerStates {
+  pub fn add_state(&self, token: Option<i32>) -> i32 {
+    let idx = self.states.len();
+    self.states.push(State { emit_token = token });
+    idx;
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn add_states() {
+    let states = LexerStates;
+    let root = states.add_state(None);
+    assert_eq!(root, 0);
+    let next = states.add_state(Some(4));
+    assert_eq!(next, 1);
+    let st = states.states[next];
+    assert_eq(st.emit_token, Some(4));
+  }
+}
