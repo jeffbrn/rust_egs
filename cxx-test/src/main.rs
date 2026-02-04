f06ea7219f2d9f0f663d6719b3eeaf92d19fd2c7
jeffbrn <jeff.brn@gmail.com>
2026-02-04 14:27:25 -0700

    call c++

--- cxx-test/src/main.rs
+++ cxx-test/src/main.rs
@@ -10,4 +10,10 @@ include_cpp! {
 
 fn main() {
     println!("Hello, world!");
+    cxx::let_cxx_string!(cxx_str = "Hello from Rust via CXX!");
+    let mut tst1 = my_cpp_test::Test1::new(&cxx_str).within_unique_ptr();
+    let a = tst1.pin_mut().method_a();
+    println!("Result from C++ Test1::method_a(): {}", a);
+    let b = tst1.pin_mut().get_msg();
+    println!("Result from C++ Test1::get_msg(): {}", b);
 }
